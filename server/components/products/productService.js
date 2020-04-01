942e0427b386b9bfb1447616a24a08a82f106165
David Nuñez <dh_nz@hotmail.com>
2020-03-31 20:51:24 -0600

    Added Components and removed the models folder.

---
+++ server/components/products/productService.js
@@ -0,0 +1,21 @@
+
+class ProductService {
+  constructor(Product) {
+    this.product = Product;
+  }
+
+  createProduct() {
+    // code goes here
+  }
+
+  updateProduct() {
+    // code goes here
+  }
+
+  listOfProducts() {
+    // return this.product.find({});
+  }
+
+}
+
+module.exports = ProductService;
