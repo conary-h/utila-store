f22514d1dea8f7284fc20ba9504814279031b4b1
David Nuñez <dh_nz@hotmail.com>
2020-04-05 22:29:07 -0600

    Created Brand, Category and Product Model Test.

--- server/components/products/productModel.test.js
+++ server/components/products/productModel.test.js
@@ -1,5 +1,11 @@
+const path = require('path');
 const mongoose = require('mongoose');
-const Product = require('productModel');
+
+const dotenvPath = path.join(__dirname, '..', '..', '.env.local');
+const dotenv = require('dotenv').config({ path: dotenvPath });
+const Product = require('./productModel');
+const Brand = require('../brand/brandModel');
+const Category = require('../categories/categoryModel');
 
 
 mongoose.connect(process.env.DATABASE_TEST, {
@@ -9,11 +15,15 @@ mongoose.connect(process.env.DATABASE_TEST, {
 
 describe('Product model', () => {
   beforeAll(async () => {
-    await Product.remove({});
+    await Product.deleteMany({});
+    await Brand.deleteMany({});
+    await Category.deleteMany({});
   });
 
   afterEach(async () => {
-    await Product.remove({});
+    await Product.deleteMany({});
+    await Brand.deleteMany({});
+    await Category.deleteMany({});
   });
 
   afterAll(async () => {
@@ -25,34 +35,53 @@ describe('Product model', () => {
   });
 
 
-  it('save and gets a product', () => {
+  it('save and gets a product', async () => {
+    const category = new Category({ categoryName: 'Electronics' });
+    const brand = new Brand({ brandName: 'Acer' });
+
+    await category.save();
+    await brand.save();
+
+    const foundCategory = await Category.findOne({ categoryName: 'Electronics' });
+    const foundBrand = await Brand.findOne({ brandName: 'Acer' });
+
     const product = new Product({
       productName: 'Macbook Pro 15',
       brandName: 'Apple',
       retailPrice: 299999,
       afterPromoRate: 289999,
       thumbnail: 'macbook.jpg',
-      brand: null,
-      category: null
+      brand: foundBrand.id,
+      category: foundCategory.id
     });
 
     await product.save();
 
     const productName = 'Macbook Pro 15';
-    const foundProduct = await Product.findOne({ productName });
+    const foundProduct = await Product.findOne({ productName: 'Macbook Pro 15' });
 
-    expect(foundProduct.name).toBe(productName)
+    expect(foundProduct).toBeDefined();
+    expect(foundProduct.productName).toBe(productName);
   });
 
-  it('update a product', () => {
+  it('update a product', async () => {
+    const category = new Category({ categoryName: 'Electronics' });
+    const brand = new Brand({ brandName: 'Acer' });
+
+    await category.save();
+    await brand.save();
+
+    const foundCategory = await Category.findOne({ categoryName: 'Electronics' });
+    const foundBrand = await Brand.findOne({ brandName: 'Acer' });
+
     const product = new Product({
       productName: 'Macbook Pro 15',
       brandName: 'Apple',
       retailPrice: 299999,
       afterPromoRate: 289999,
       thumbnail: 'macbook.jpg',
-      brand: null,
-      category: null
+      brand: foundBrand.id,
+      category: foundCategory.id
     });
 
     await product.save();
